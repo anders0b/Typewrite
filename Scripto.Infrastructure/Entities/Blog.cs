f45f8c363bd6b57f847b75e5287aa3da0c70105b
Anders Ö <anders.oberg@iths.se>
2024-05-21 11:25:38 +0200

    Add project files.

---
+++ Scripto.Infrastructure/Entities/Blog.cs
@@ -0,0 +1,9 @@
+﻿namespace Typewrite.Infrastructure.Entities;
+
+public class Blog
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Url { get; set; }
+    public List<Post> Posts { get; set; }
+}
