3a40fc77c7eb1a311f83d98d7ec2a752da7a540f
Anders Ö <anders.oberg@iths.se>
2024-11-01 10:00:01 +0100

    added repository class

---
+++ src/Typewrite.Infrastructure/AuthorRepository.cs
@@ -0,0 +1,13 @@
+﻿using Typewrite.Infrastructure.Entities;
+
+namespace Typewrite.Infrastructure;
+
+public interface IAuthorRepository
+{
+    public Task CreateAuthorAsync(Author author);
+    public Task<Author> GetAuthorAsync(Guid id);
+    public Task<List<Author>> GetAuthorsAsync();
+    public Task UpdateAuthorAsync(Author author);
+    public Task DeleteAuthorAsync(Guid id);
+    public Task<Author> GetAuthorByIdAsync(Guid id);
+}
