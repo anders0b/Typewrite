7527fac330f8c8748bc9136b6f5cba6bdb3329a9
Anders Ö <anders.oberg@iths.se>
2024-11-01 09:50:37 +0100

    added entities, dbup and dapper

---
+++ src/Typewrite.Infrastructure/BlogRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using Typewrite.Infrastructure.Entities;
+
+namespace Typewrite.Infrastructure;
+
+public interface IBlogRepository
+{
+    public Task CreateBlogAsync(Blog blog);
+    public Task<Blog> GetBlogAsync(Guid id);
+    public Task<List<Blog>> GetBlogsAsync();
+    public Task UpdateBlogAsync(Blog blog);
+    public Task DeleteBlogAsync(Guid id);
+    public Task<Blog> GetBlogByIdAsync(Guid id);
+}
+public class BlogRepository : IBlogRepository
+{
+    private readonly string _connectionString;
+    public BlogRepository(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection");
+    }
+    public Task CreateBlogAsync(Blog blog)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteBlogAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Blog> GetBlogAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Blog> GetBlogByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Blog>> GetBlogsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateBlogAsync(Blog blog)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Typewrite.Infrastructure/MigrationHelper.cs
@@ -0,0 +1,31 @@
+﻿using DbUp;
+using Microsoft.Extensions.Logging;
+
+namespace Typewrite.Infrastructure;
+
+public static class MigrationHelper
+{
+    public static void EnsureDatabaseIsAvailableAndUpToDate(string connectionString, ILogger logger)
+    {
+        EnsureDatabase.For.SqlDatabase(connectionString);
+        var upgradeEngine = 
+            DeployChanges.To
+            .SqlDatabase(connectionString)
+            .WithScriptsEmbeddedInAssembly(typeof(MigrationHelper).Assembly)
+            .LogToAutodetectedLog()
+            .Build();
+        if(upgradeEngine.GetScriptsToExecute().Any())
+        {
+            foreach(var script in upgradeEngine.GetScriptsToExecute())
+            {
+                logger.LogInformation($"Executing script: {script.Name}");
+            }
+            var dbUpgradeResult = upgradeEngine.PerformUpgrade();
+            if(!dbUpgradeResult.Successful)
+            {
+                logger.LogError("Database upgrade failed");
+                throw new Exception($"Database upgrade failed {dbUpgradeResult.Error.Message}", dbUpgradeResult.Error);
+            }
+        }
+    }
+}
